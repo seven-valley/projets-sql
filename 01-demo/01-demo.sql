a4d3010fd17eb73b4fcd859e115b5d55b09ec520
Jeff <augure@gmail.com>
2024-04-02 14:08:53 +0200

    premier Tp sql

---
+++ 01-demo/01-demo.sql
@@ -0,0 +1,27 @@
+-- commentaires
+# commentaires
+-- Norme d'ecriture
+-- nom base
+-- nom des tables
+-- nom des champs
+
+-- Snake cake
+-- ma_super_base_de_donnees
+
+--camel case
+-- maSuperBaseDeDonnees
+
+-- pascal case en programmation : nom des classe
+-- MaSuperClass
+
+-- kebab case 
+-- ma-super-page.html
+
+--CREATE DATABASE ma_base;
+
+--CREATE DATABASE videotheque CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+--DROP DATABASE videotheque;
+
+USE videotheque;
+CREATE TABLE film (...) ENGINE=InnoDB; 
